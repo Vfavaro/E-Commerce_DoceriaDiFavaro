199926ede29e744bdfd85138830a96a406bd7feb
Vitória Espíndola Fávaro <146202522+Vfavaro@users.noreply.github.com>
2023-12-06 11:16:02 -0300

    Add files via upload

---
+++ Doceria_diFavaro/Doceria_diFavaro/JavaScript/produto_bento_cake.js
@@ -0,0 +1,24 @@
+import {products} from "./data.js"
+
+function carregar(lista_produtos){
+
+    lista_produtos.forEach(produto => {
+        let cod = ` 
+                <div class="product" id="${produto.id}">
+
+                <h2>${produto.title}</h2>
+                
+                <img src="${produto.poster}" alt="${produto.id}">
+
+                <p>${produto.price}</p>
+                
+            </div>
+        `;
+        let elem = document.querySelector(".products");
+        elem.innerHTML += cod
+    });
+   
+}
+
+let listaBentoCake = products.filter(produto => produto.categories === "Bento Cake")
+carregar(listaBentoCake)
